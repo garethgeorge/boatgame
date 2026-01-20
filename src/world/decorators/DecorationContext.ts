5c7b30c187bebe0a793a58bed3488f6e5d2ac3c3
Paul George <rdlp32@gmail.com>
2026-01-20 10:34:32 -0800

    Reorganize class members for readability

--- src/world/decorators/DecorationContext.ts
+++ src/world/decorators/DecorationContext.ts
@@ -6,25 +6,28 @@ import { BiomeDecorationHelper } from '../biomes/BiomeDecorationHelper';
 export interface DecorationContext {
     // The chunk we are decorating
     chunk: TerrainChunk;
-    // The river system
-    riverSystem: RiverSystem;
-    // Output for geometries that are grouped by material. These can be
-    // merged for better performance. 
-    geometriesByMaterial: Map<THREE.Material, THREE.BufferGeometry[]>;
-    // Output for instanced data. Map<Geometry, Map<Material, { matrix: THREE.Matrix4, color?: THREE.Color }[]>>
-    instancedData: Map<THREE.BufferGeometry, Map<THREE.Material, { matrix: THREE.Matrix4, color?: THREE.Color }[]>>;
-    // Layout data for the current biome segment (contains boat path and static decoration manifests)
-    layout?: any;
-    // Output for geometries that can't be grouped by material.
-    geometryGroup: THREE.Group;
-    // Output for animation mixers
-    animationMixers: THREE.AnimationMixer[];
     // The z offset of the chunk
     zOffset: number;
     // The start Z of the current biome
     biomeZStart: number;
     // The end Z of the current biome
     biomeZEnd: number;
+
+    // The river system
+    riverSystem: RiverSystem;
+    // Layout data for the current biome segment (contains boat path and static decoration manifests)
+    layout?: any;
+
     // Helper for biome decorations
     decoHelper: BiomeDecorationHelper;
+
+    // Output for geometries that are grouped by material. These can be
+    // merged for better performance. 
+    geometriesByMaterial: Map<THREE.Material, THREE.BufferGeometry[]>;
+    // Output for instanced data. Map<Geometry, Map<Material, { matrix: THREE.Matrix4, color?: THREE.Color }[]>>
+    instancedData: Map<THREE.BufferGeometry, Map<THREE.Material, { matrix: THREE.Matrix4, color?: THREE.Color }[]>>;
+    // Output for geometries that can't be grouped by material.
+    geometryGroup: THREE.Group;
+    // Output for animation mixers
+    animationMixers: THREE.AnimationMixer[];
 }
