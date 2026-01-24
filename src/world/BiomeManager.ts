3d89343f931df46d5c237d8143669dfa5f90e05e
Paul George <rdlp32@gmail.com>
2026-01-24 14:30:33 -0800

    Oops

--- src/world/BiomeManager.ts
+++ src/world/BiomeManager.ts
@@ -19,7 +19,7 @@ interface BiomeInstance {
 }
 
 export class BiomeManager {
-  public static DEBUG_BIOME = true;
+  public static DEBUG_BIOME = false;
 
   private biomeInstances: BiomeInstance[] = [];
   private totalSequenceLength = 0;
